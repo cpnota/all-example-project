1073edcd3564820dd06289488e52e22b149f1b6b
Chris Nota <cpnota@gmail.com>
2020-01-11 12:53:18 -0500

    add comments

--- agent.py
+++ agent.py
@@ -4,6 +4,26 @@ from all.agents import Agent
 
 
 class ModelPredictiveDQN(Agent):
+    """
+    Model Predictive DQN.
+    This is a simplified model predictive control algorithm based on DQN.
+    The purpose of this agent is to demonstrate how the autonomous-learning-library
+    can be used to build new types of agents from scratch, while reusuing many
+    of the useful features of the library. This agent selects actions by predicting
+    future states conditioned on each possible action choosing the action
+    with the highest expected return based on this prediction. It trains on a replay
+    buffer in a style similar to DQN.
+
+    Args:
+        f (FeatureNetwork): Shared feature layers.
+        v (VNetwork): State-value function head.
+        r (QNetwork): Reward prediction head.
+        g (Approximation): Transition model.
+        replay_buffer (ReplayBuffer): Experience replay buffer.
+        discount_factor (float): Discount factor for future rewards.
+        minibatch_size (int): The number of experiences to sample in each training update.
+        replay_start_size (int): Number of experiences in replay buffer when training begins.
+    """
     def __init__(self,
                  f, # shared feature representation
                  v, # state-value head
@@ -29,6 +49,13 @@ class ModelPredictiveDQN(Agent):
         self._action = None
 
     def act(self, state, reward):
+        """
+        Update the agent and choose a new action.
+
+        Args:
+            state (State): The current environment state.
+            reward (float): The reward for the previous state-action pair.
+        """
         self.replay_buffer.store(self._state, self._action, reward, state)
         self._train()
         self._state = state
@@ -36,6 +63,11 @@ class ModelPredictiveDQN(Agent):
         return self._action
 
     def _choose_action(self, state):
+        """
+        Choose the best action in the current state using our model predictions.
+        Note that every call below uses .eval(), which puts torch in no_grad mode,
+        and sometimes changes their behavior.
+        """
         features = self.f.eval(state)
         predicted_rewards = self.r.eval(features)
         predicted_next_states = self.g.eval(features)
@@ -44,6 +76,7 @@ class ModelPredictiveDQN(Agent):
         return torch.argmax(predicted_returns, dim=0)
 
     def _train(self):
+        """Update the agent."""
         if len(self.replay_buffer) > self.replay_start_size:
             # sample transitions from buffer
             (states, actions, rewards, next_states, _) = self.replay_buffer.sample(self.minibatch_size)
